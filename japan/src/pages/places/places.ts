ea83b7231de9971a63eb3dd0a0b9980bc603d07b
MCollins434 <michael.collins08@gmail.com>
2017-08-27 20:49:27 -0400

    4.2

---
+++ japan/src/pages/places/places.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+import Place from '../../models/place';
+import { DataProvider } from '../../providers/data';
+
+@Component({
+  selector: 'page-places',
+  templateUrl: 'places.html',
+})
+export class PlacesPage {
+  places: Place[];
+  tokyoPlaces: Place[];
+  hakonePlaces: Place[];
+  hiroshimaPlaces: Place[];
+  kyotoPlaces: Place[];
+  otherPlaces: Place[];
+
+  tokyo: string = "TOKYO";
+  hakone: string = "HAKONE";
+  hiroshima: string = "HIROSHIMA";
+  kyoto: string = "KYOTO";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public data: DataProvider) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PlacesPage');
+    this.data.getPlaces().then((response) => {
+      this.places = response;
+      this.tokyoPlaces = this.places.filter((place) => place.city.toUpperCase() == this.tokyo.toUpperCase());
+      this.hakonePlaces = this.places.filter((place) => place.city.toUpperCase() == this.hakone.toUpperCase());
+      this.hiroshimaPlaces = this.places.filter((place) => place.city.toUpperCase() == this.hiroshima.toUpperCase());
+      this.kyotoPlaces = this.places.filter((place) => place.city.toUpperCase() == this.kyoto.toUpperCase());
+      this.otherPlaces = this.places.filter((place) => 
+      place.city.toUpperCase() != this.tokyo.toUpperCase() &&
+      place.city.toUpperCase() != this.hakone.toUpperCase() &&
+      place.city.toUpperCase() != this.hiroshima.toUpperCase() &&
+      place.city.toUpperCase() != this.kyoto.toUpperCase() );
+    });
+  }
+
+}
